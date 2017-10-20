eb47f1d287c16006b8b0189575089eedddf42f3a
Anda Nachai <tn.pechii@gmail.com>
2017-10-20 14:09:07 +0700

    Anda 14:08 20/10/2560

--- src/app/services/sponsors.service.ts
+++ src/app/services/sponsors.service.ts
@@ -1,3 +1,4 @@
+import { config } from './../app.config';
 import { SmSponsors } from './../content/models/sm-sponsors';
 import { SponsorsForm } from './../content/form/sponsors-form';
 import { Observable } from 'rxjs';
@@ -5,17 +6,21 @@ import 'rxjs/Rx'  // import for .map; used for every services
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
 
-const url = 'http://127.0.0.1:8000/sponsors';
 
 @Injectable()
 export class SponsorsService {
+
+  
+
+  private mainUrl: string = config.backendUrl;
+  url = this.mainUrl + 'sponsors';
 
   constructor(private http: Http) { }
 
   getSponsors(): Observable<SmSponsors[]> {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
-    return this.http.get(url, options)
+    return this.http.get(this.url, options)
       .map(
       (res: Response) => {
         return res.json();
@@ -27,7 +32,7 @@ export class SponsorsService {
     const body = JSON.stringify(sponsors);
     console.log(body);
     const headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.post(url, body, { headers: headers });
+    return this.http.post(this.url, body, { headers: headers });
   }
 
   searchSponsors(sponsors: SponsorsForm): Observable<SponsorsForm[]> {
@@ -50,7 +55,7 @@ export class SponsorsService {
 
     console.log(body);
     console.log(criteria);
-    return this.http.get(url + criteria, { headers: headers })
+    return this.http.get(this.url + criteria, { headers: headers })
       .map(
       (res: Response) => {
         let results: SponsorsForm[] = [];
@@ -79,7 +84,7 @@ export class SponsorsService {
     const body = JSON.stringify(sponsors);
     console.log(body);
     const headers = new Headers({ 'Content-Type': 'application/json' });
-    return this.http.put(url + ref, body, { headers: headers });
+    return this.http.put(this.url + ref, body, { headers: headers });
   }
 
 }
