690e4bc9ce6117a17613a24b38c8804e8b631358
Pariwat Viriyaphan <aigbolo@gmail.com>
2017-11-10 16:55:47 +0700

    update stylesheet UI

--- src/app/content/pages/family-and-address/sibling/sibling.component.ts
+++ src/app/content/pages/family-and-address/sibling/sibling.component.ts
@@ -1,11 +1,12 @@
 import { AcSibling } from './../../../models/ac-sibling';
 import { FamilyAndAddressComponent } from './../family-and-address.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-sibling',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './sibling.component.html',
-  styleUrls: ['../family-and-address.component.css' , '../../pages.component.css']
+  styleUrls: ['./sibling.component.css' , '../family-and-address.component.css' , '../../pages.component.css']
 })
 export class SiblingComponent implements OnInit {
 
@@ -18,14 +19,15 @@ export class SiblingComponent implements OnInit {
     this.sibling.born_year = '2560';
     this.siblings = [];
     this.siblings.push(this.sibling);
+    this.sibling = new AcSibling();
     this.siblings.push(this.sibling);
-
     console.log('siblings: '+this.siblings.length);
   }
 
   nextButtonOnClick(){
     console.log('nextButtonOnClick');
-    this.familyAndAddress.onNext(2);
+    // this.familyAndAddress.onNext(1);
+    console.log(this.siblings);
   }
   prevButtonOnClick(){
     console.log('prevButtonOnClick');
