b01dbf6ed0f59463d2f6929926ac0189886de5b7
littlesquid <littlesquid.1991@gmail.com>
2017-11-21 10:48:04 +0700

    noei

---
+++ src/app/content/pages/document-request/document-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-request',
+  templateUrl: './document-request.component.html',
+  styleUrls: ['./document-request.component.css']
+})
+export class DocumentRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
